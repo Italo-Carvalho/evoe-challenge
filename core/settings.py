8f6884eb5957b7bcac664fbc95b789324bc29e87
Ítalo Carvalho <italocarvalhoti@hotmail.com>
2021-07-16 16:07:17 -0300

    api pagination

--- core/settings.py
+++ core/settings.py
@@ -26,13 +26,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # internal
-    'app',
+    'app.apps.AppConfig',
     # external
     'rest_framework',
     'rest_framework.authtoken',
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
